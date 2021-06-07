f28dc6110e7232011caeed9767e77a981a0b3373
Kybre <hermanchan10@gmail.com>
2021-06-06 23:37:00 -0700

    fixed URL links

--- content.js
+++ content.js
@@ -4,35 +4,35 @@ let data = [
 		title:"Scrapbook",
 		ctgr:"UI/UX Project",
 		content:"A children's mental health journal created during Hack4Health 2021.",
-		link:"scrapbook.html",
+		link:"scrapbook",
 	},
 	{
 		img: "./img/EiSC/title.png",
 		title: "Emergency in Section C",
 		ctgr: "Game Design/Development Project",
 		content: "A emergency-themed platformer developed in Unity for the May 2021 Make-A-Level Game Jam.",
-		link: "emergency-in-section-c.html"
+		link: "emergency-in-section-c"
 	},
 	{
 		img: "./img/Kingslayer/Kingslayer.png",
   	title: "Kingslayer",
 		ctgr: "Game Design/Development Project",
     content: "A high-action, platformer bullet hell game developed in Processing.",
-		link: "kingslayer.html"
+		link: "kingslayer"
   },
 	{
 		img: "./img/KMSRT/kmsrt.png",
 		title: "Kalmont Mountain Safety and Rescue Team Website",
 		ctgr: "Web Design/Development Project",
 		content: "A fictional website for the Kalmont Mountain Safety and Rescue Team Website containing local news and tips and tricks on winter outdoors safety.",
-		link: "kmsrt.html"
+		link: "kmsrt"
 	},
   {
 		img: "./img/MineDiamonds/MineDiamonds.png",
   	title: "Mine Diamonds",
 		ctgr: "Arduino Design/Development Project",
     content: "An interactive application made with Arduino and Processing.",
-		link: "minediamonds.html"
+		link: "minediamonds"
   }
 ]
 
