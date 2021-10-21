ea15cd3bf04875244cadc181cd850514d87cc2ad
Kybre <hermanchan10@gmail.com>
2021-10-21 15:54:59 -0700

    seperated projects by UX/Game, added About info to landing screen

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
-let data = [
+let uxData = [
+	{
+		img:"./img/Scrapbook/title.png",
+		title:"Scrapbook",
+		ctgr:"UI/UX Project",
+		content:"A children's mental health journal created during Hack4Health 2021.",
+		tools:"Figma",
+		link:"scrapbook",
+	},
+	{
+		img: "./img/KMSRT/kmsrt.png",
+		title: "Kalmont Mountain Safety and Rescue Team Website",
+		ctgr: "Web Design/Development Project",
+		content: "A fictional website for the Kalmont Mountain Safety and Rescue Team Website containing local news and tips and tricks on winter outdoors safety.",
+		tools:"HTML/CSS",
+		link: "kmsrt"
+	},
+]
+
+let gameData = [
 	{
 		img:"./img/Unfabled/cover.png",
 		title:"Unfabled",
 		ctgr:"Game Design/Development Project",
 		content:"A fast-paced, spell-slinging action game developed in Unity for Ludum Dare 49.",
+		tools:"Unity, C#",
 		link:"unfabled",
 	},
 	{
@@ -11,6 +31,7 @@ let data = [
 		title:"Monkey Mobius",
 		ctgr:"Game Design/Development Project",
 		content:"An infinite-themed puzzle game developed in Unity for the 2021 BC Game Jam. \n Winner of the Participant's Choice award.",
+		tools:"Unity, C#",
 		link:"monkey-mobius",
 	},
 	{
@@ -18,41 +39,38 @@ let data = [
 		title:"Hell Gene",
 		ctgr:"Game Design/Development Project",
 		content:"A chaos-themed, top-down hack and slash developed in Unity for Brackey's Game Jam 2021.2.",
+		tools:"Unity, C#",
 		link:"hell-gene",
-	},
-	{
-		img:"./img/Scrapbook/title.png",
-		title:"Scrapbook",
-		ctgr:"UI/UX Project",
-		content:"A children's mental health journal created during Hack4Health 2021.",
-		link:"scrapbook",
 	},
 	{
 		img: "./img/EiSC/title.png",
 		title: "Emergency in Section C",
 		ctgr: "Game Design/Development Project",
 		content: "An emergency-themed platformer developed in Unity for the May 2021 Make-A-Level Game Jam.",
+		tools:"Unity, C#",
 		link: "emergency-in-section-c"
 	},
 	{
 		img: "./img/Kingslayer/Kingslayer.png",
-  	title: "Kingslayer",
+		title: "Kingslayer",
 		ctgr: "Game Design/Development Project",
-    content: "A high-action, platformer bullet hell game developed in Processing.",
+		content: "A high-action, platformer bullet hell game developed in Processing.",
+		tools:"Processing",
 		link: "kingslayer"
-  },
-	{
-		img: "./img/KMSRT/kmsrt.png",
-		title: "Kalmont Mountain Safety and Rescue Team Website",
-		ctgr: "Web Design/Development Project",
-		content: "A fictional website for the Kalmont Mountain Safety and Rescue Team Website containing local news and tips and tricks on winter outdoors safety.",
-		link: "kmsrt"
 	},
 ]
 
-const parent = document.querySelector("#main");
+uxData.forEach((data) => {
+	const parent = document.querySelector("#ux");
+	appendContent(parent, data);
+})
 
-data.forEach(({img, title, ctgr, content, link}) => {
+gameData.forEach((data) => {
+	const parent = document.querySelector("#game");
+	appendContent(parent, data);
+})
+
+function appendContent(parent, {img, title, ctgr, content, tools, link}){
 	let div = document.createElement("a");
 	let contentDiv = document.createElement("div");
 	div.href = link;
@@ -72,6 +90,12 @@ data.forEach(({img, title, ctgr, content, link}) => {
 
 	let txt = document.createElement("div");
 	txt.innerText = content;
+
+	let tol = document.createElement("div");
+	tol.innerText = "Tools Used: " + tools;
+	tol.style.fontStyle = "italic";
+	tol.style.marginBottom = "1rem";
+	tol.style.marginTop = "1rem";
 	/*
 	<img src="img/Kingslayer.png">
 	<a href="page.html" class="item-content">
@@ -84,5 +108,6 @@ data.forEach(({img, title, ctgr, content, link}) => {
 	contentDiv.appendChild(proj);
 	contentDiv.appendChild(cat);
 	contentDiv.appendChild(txt);
-  parent.appendChild(div);
-})
+	contentDiv.appendChild(tol);
+	parent.appendChild(div);
+}
